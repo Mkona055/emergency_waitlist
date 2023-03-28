a23464506b46e1fee724c41862ffcb6baf67d1eb
somakonate <mkona055@uottawa.ca>
2023-03-27 22:47:45 -0400

    update schema

--- schema.sql
+++ schema.sql
@@ -2,13 +2,13 @@ CREATE TABLE IF NOT EXISTS patients (
     id VARCHAR(255) PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    injury_severity INTEGER NOT NULL CHECK (injury >= 0 AND injury <= 4),
+    injury_severity INTEGER NOT NULL CHECK (injury_severity >= 0 AND injury_severity <= 4),
     code CHAR(3) UNIQUE NOT NULL,
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     came_at TIMESTAMP NOT NULL DEFAULT NOW(),
     served BOOLEAN NOT NULL DEFAULT FALSE
-)
+);
 
 INSERT INTO patients (id, first_name, last_name, injury_severity, code, email, phone) 
 VALUES  ('1', 'John', 'Doe', 1, 'ABC', 'john@gmail.com', '1234567890'),
