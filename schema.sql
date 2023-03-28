6d64444f35336435e1b2eb13db448c9c8c25e77d
somakonate <mkona055@uottawa.ca>
2023-03-28 15:17:12 -0400

    edit schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS patients (
-    id VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY DEFAULT gen_random_uuid(),
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     injury_severity INTEGER NOT NULL CHECK (injury_severity >= 0 AND injury_severity <= 4),
@@ -10,8 +10,17 @@ CREATE TABLE IF NOT EXISTS patients (
     served BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+CREATE TABLE IF NOT EXISTS admins (
+    id VARCHAR(255) PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
 INSERT INTO patients (id, first_name, last_name, injury_severity, code, email, phone) 
-VALUES  ('1', 'John', 'Doe', 1, 'ABC', 'john@gmail.com', '1234567890'),
-        ('2', 'Jane', 'Doe', 2, 'DEF', 'Jane@gmail.com', '1234567890'),
-        ('3', 'John', 'Smith', 3, 'GHI', 'jsmith@gmail.com', '1234567890'),
-        ('4', 'Jane', 'Smith', 4, 'JKL', 'janesmith@gmail.com', '1234567890')
+VALUES  ('1', 'John', 'Doe', 1, 'ABC', 'john@gmail.com', '(623)-656-9590'),
+        ('2', 'Jane', 'Doe', 2, 'DEF', 'Jane@gmail.com', '(123)-466-9890'),
+        ('3', 'John', 'Smith', 3, 'GHI', 'jsmith@gmail.com', '(523)-656-7890'),
+        ('4', 'Jane', 'Smith', 4, 'JKL', 'janesmith@gmail.com', '(629)-556-7890')
+
+INSERT INTO admins (username, password) 
+VALUES  ('admin', 'admin');
