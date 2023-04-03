b8f5ea54a1f93f7a25c0eddd1cdd05862c0f9ab5
somakonate <mkona055@uottawa.ca>
2023-04-03 02:33:59 -0400

    add bootstrap js

--- public/admin/addpatient.php
+++ public/admin/addpatient.php
@@ -27,9 +27,10 @@ if (!isset($_SESSION['admin'])) {
             <meta name="viewport" content="width=device-width, initial-scale=1">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
                 integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
-            <script src="/scripts/admin.js"></script>
+            <script src="../scripts/admin.js"></script>
 
         </head>
 
