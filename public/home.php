c8c92fa2ae65bdab35e1cae134afccf993b9a47a
somakonate <mkona055@uottawa.ca>
2023-04-03 02:35:05 -0400

    add jquery

--- public/home.php
+++ public/home.php
@@ -20,7 +20,9 @@
                 <link rel="stylesheet" href="../styles/index.css">
 
                 <!-- Bootstrap JS -->
-                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.inputmask/3.3.4/jquery.inputmask.bundle.min.js"></script>
             </head>
             <body>
                 <nav class="d-flex navbar navbar-expand-lg navbar-light border border-black bg-light ps-2">
