c3af9da090bdb21bc20ccb2d0005d154a3b8311e
somakonate <mkona055@uottawa.ca>
2023-04-04 13:12:45 -0400

    create db docs

--- docs/db/schema.sql
+++ docs/db/schema.sql
@@ -16,11 +16,3 @@ CREATE TABLE IF NOT EXISTS admins (
     password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO patients (id, first_name, last_name, injury_severity, code, email, phone) 
-VALUES  ('1', 'John', 'Doe', 1, 'ABC', 'john@gmail.com', '(623)-656-9590'),
-        ('2', 'Jane', 'Doe', 2, 'DEF', 'Jane@gmail.com', '(123)-466-9890'),
-        ('3', 'John', 'Smith', 3, 'GHI', 'jsmith@gmail.com', '(523)-656-7890'),
-        ('4', 'Jane', 'Smith', 4, 'JKL', 'janesmith@gmail.com', '(629)-556-7890')
-
-INSERT INTO admins (username, password) 
-VALUES  ('admin', 'admin');
