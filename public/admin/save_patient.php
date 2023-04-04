f02a6c0cb58e37879e02264e23eb2559201790dc
somakonate <mkona055@uottawa.ca>
2023-04-04 13:11:50 -0400

    add code on patient succesly saved message

--- public/admin/save_patient.php
+++ public/admin/save_patient.php
@@ -3,12 +3,12 @@ require_once('../_config.php');
 use Models\Patient;
 
 if (isset($_POST) && !empty($_POST)) {
-    $dbconn = pg_connect("host=localhost dbname=postgres user=postgres password=postgres");
+    $dbconn = pg_connect("host=localhost dbname=emergency_waitlist user=postgres password=postgres");
     $form = $_POST;
     $patient = Patient::getPatientfromForm($form);
     if ($patient->save($dbconn)) {
         $saved = true;
-        header("Location: ./addpatient.php?success=true");
+        header("Location: ./addpatient.php?success=true&code=" . $patient->code);
 
     } else {
         $error = true;
